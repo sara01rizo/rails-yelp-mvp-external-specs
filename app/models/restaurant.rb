c87902c40af65eb00bc6f0cfb7570027f3d3ed68
sara pulido <sara01rizo@gmail.com>
2022-10-11 21:10:14 +0200

    Create: restaurants controller and reviews controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :name, presence: true
+  validates :category, inclusion: {in: CATEGORY}
+
 end
