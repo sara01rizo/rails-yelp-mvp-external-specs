f5b62fdcf5d1d1252d0a8ba5f1e4838c75885142
sara pulido <sara01rizo@gmail.com>
2022-10-13 18:48:23 +0200

    Add: restaurants controller file

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,41 @@
 class RestaurantsController < ApplicationController
-
   def index
     @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Pet.find(params[:id])
+    @restaurant.update(pet_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:pet).permit(:name, :address, :phone_number, :category)
   end
 end
